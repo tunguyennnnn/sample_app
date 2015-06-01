166a4b23ad370288e43e077dc7e4cf1b004b2599
Tu Nguyen <conghoa522@gmail.com>
2015-06-01 19:59:27 -0400

    Fisnish user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update]
-  before_fileter :admin_user, only: :destroy
+  before_filter :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
